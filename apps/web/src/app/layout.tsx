5816063ebb010cf007a8ab0a8f5816775a8b230f
Markimg22 <marquim.net.221200@hotmail.com>
2026-02-09 11:35:34 -0400

    chore(web): implement @monorepo/ui in app web application

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
-import './globals.css';
+
+import '@monorepo/ui/styles.css';
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
     title: 'Web',
